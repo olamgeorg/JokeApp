552591428e708beae702a8f54fb43df859a50351
olamgeorg <olamgeorg9@gmail.com>
2024-07-23 14:34:41 +0100

    initial commit

---
+++ Api.js
@@ -0,0 +1,31 @@
+//two types of Api
+// install to your extention = THUNDER CLIENT API.
+// on your BROWSER type = v2.jokeapi.client
+//FREE API and PAID they are called REST.
+//api for weathers,joke,geting country,character,movies.
+
+// NOTE!
+//3 method of creating API= axios,fetch and asink await
+
+
+
+
+const url = 'https://v2.jokeapi.dev/joke/Any'
+const joke = document.getElementById('joke')
+const buton = document.getElementById('buton');
+
+
+let getJoke = ()=> { 
+fetch (url).then(data => data.json())
+.then((item)=> 
+{
+   // (console.log(item.joke)
+
+    joke.textContent = `${item.joke}`
+}
+  );
+
+}
+buton.addEventListener('click',getJoke());
+getJoke();
+     
